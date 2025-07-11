be4fb5ba90ac8c594ef7e20e7ae54c438a754988
Dietterich <ldietteric@haverford.edu>
2025-07-11 14:19:43 -0400

    Finished drafting desiccation function.

--- R/desiccation.R
+++ R/desiccation.R
@@ -1,22 +1,21 @@
 #' Calculate macrophyte losses to desiccation
 #'
-#' \code{desiccation} calculatesm acrophyte losses due to desiccation.
+#' \code{desiccation} calculates macrophyte losses due to desiccation.
 #' 
 #' @param old.size size at prior time step (e.g., biomass or stem length).
-#' @param depth water depth at current time step.
-#' @param depth.limit shallowest water depth the macrophyte can tolerate without experiencing desiccation.
-#' @param dry.time current length of time the macrophyte has spent in water shallower than depth.limit.
-#' @param dry.time.limit length of time the macrophyte can tolerate in water shallower than depth.limit without experiencing desiccation.
 #' @param D desiccation rate in proportion lost per day.
-#'
+#' @param depth a vector of water depths at the last N time steps, where N = dry.time.limit 
+#' @param depth.limit shallowest water depth the macrophyte can tolerate without experiencing desiccation.
+#' @param dry.time.limit number of time steps the macrophyte can tolerate at water depths shallower than depth.limit without experiencing desiccation.
+#' 
 #' @return daily macrophyte loss due to desiccation, a positive number.
 #' 
 #' @details 
 #' This function calculates macrophyte size (e.g., stem length or biomass) lost due to desiccation in a 
 #' given time step. Desiccation is assumed to reduce macrophyte size at rate D whenever water is shallower 
-#' than a given depth (depth.limit) for a given amount of time (dry.time.limit) or longer. All measures of 
-#' macrophyte size must use the same units. Podostemum, like many macrophytes, has been shown to be 
-#' sensitive to desiccation (Wood and Freeman 2017).
+#' than a given depth (depth.limit) for at least a set number of time steps (the length of the depth input). 
+#' All measures of macrophyte size must use the same units. Podostemum, like many macrophytes, has been 
+#' shown to be sensitive to desiccation (Wood and Freeman 2017).
 #' 
 #' @references
 #'  Wood, James, and Mary Freeman. “Ecology of the Macrophyte Podostemum Ceratophyllum Michx. (Hornleaf 
@@ -24,45 +23,38 @@
 #'  (March 2017): 65–74. https://doi.org/10.1016/j.aquabot.2017.02.009.
 #' 
 #' @examples
-#' #Result: macrophyte biomass loss of 5 g
-#' scour(old.size=10, size.min=2, S=0.5, Vlow=2.0, Vhigh=3.5, V=2.5, type=1)
+#' #Result: macrophyte biomass loss of 0 g
+#' desiccation(old.size=10, D=.4, depth=2, depth.limit=1)
+#' 
+#' #Result: macrophyte biomass loss of 4 g
+#' desiccation(old.size=10, D=.4, depth=1, depth.limit=2)
 #' 
 #' #Result: macrophyte biomass loss of 0 g
-#' scour(old.size=0.8, size.min=2, S=0.5, Vlow=2.0, Vhigh=3.5, V=2.5, type=2)
+#' desiccation(old.size=10, D=.4, depth=c(1.3, 1.2, 1.1, 1), depth.limit=0.5)
 #' 
-#' #Result: macrophyte biomass loss of 0.4 g
-#' scour(old.size=0.8, size.min=2, S=0.5, Vlow=2.0, Vhigh=3.5, V=3.6, type=2)
+#' #Result: macrophyte stem length loss of 4 cm
+#' desiccation(old.size=10, D=.4, depth=c(1.3, 1.2, 1.1, 1), depth.limit=2)
 #' 
-#' #Result: macrophyte stem length loss of 0 cm
-#' scour(old.size=10, size.min=2, S=0.6, Vlow=2.0, Vhigh=3.5, V=1, type=3)
+# #Result: macrophyte stem length loss of 0 cm
+#' desiccation(old.size=10, D=.4, depth=c(1.3, 1.2, 2.1, 1), depth.limit=2)
 #' 
-#' #Result: macrophyte stem length loss of 2 cm
-#' scour(old.size=10, size.min=2, S=0.6, Vlow=2.0, Vhigh=3.5, V=2.5, type=3)
-#' 
-#' #Result: macrophyte stem length loss of 6 cm
-#' scour(old.size=10, size.min=2, S=0.6, Vlow=2.0, Vhigh=3.5, V=3.6, type=3)
+#' #Result: macrophyte stem length loss of 12 cm
+#' desiccation(old.size=15, D=.8, depth=0, depth.limit=.5)
 #' 
 #' @export
-scour <- function(old.size, size.min, S, Vlow, Vhigh, V, type){
+
+desiccation <- function(old.size, D, depth, depth.limit){
   #Error handling for invalid model specification
-  if(!(type %in% seq(1,3))){stop("Invalid model specification")}
-  if(S < 0 | S > 1){stop("S must be between 0 and 1")}
-  if(is.logical(old.size) || is.logical(S) || is.logical(Vlow) || is.logical(Vhigh) || is.logical(V) || is.logical(type)){stop("Invalid input")}
-  if(old.size < 0 || size.min < 0){stop("Macrophyte size cannot be negative")}
-  if(Vlow < 0 || Vhigh < 0 || V < 0){stop("Velocity cannot be negative")}
-  if(Vlow > Vhigh){stop("Vlow must be less than or equal to Vhigh")}
+  if(D < 0 | D > 1){stop("D must be between 0 and 1")}
+  if(is.logical(old.size) || is.logical(D) || is.logical(depth) || is.logical(depth.limit)){stop("Invalid input")}
+  if(old.size < 0){stop("Macrophyte size cannot be negative")}
   
-  #Computation of size (e.g. biomass) lost to scour
-  #Type 1: lose constant proportion of size
-  else if(type == 1){scour.loss <- S * old.size}
+  #Computation of size (e.g., biomass) lost to desiccation
+  #Desiccation occurs if depth is less than depth.limit
+  else if(all(depth < depth.limit)){dry.loss <- D * old.size}
+  #Otherwise desiccation does not affect macrophyte size
+  else {dry.loss <- 0}
   
-  #Type 2: lose constant proportion of size when velocity above threshold, otherwise no scour
-  else if(type == 2){scour.loss <- ifelse(V > Vhigh, S * old.size, 0)}
-  
-  #Type 3: step function: lose no biomass when V < Vlow, lose maximum proportion of biomass when V > Vhigh, and transition linearly between them.
-  else if(type == 3){scour.loss <- ifelse(V < Vlow, 0,
-                                          ifelse(V < Vhigh, ((S/(Vhigh-Vlow))*V+(S*Vlow/(Vlow-Vhigh))) * old.size, S * old.size))}
-  
-  #Return size increment (a positive number).
-  return(scour.loss)
-}
+  #Return biomass increment (a positive number).
+  return(dry.loss)
+} #End fxn call
